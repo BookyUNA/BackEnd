02210a543289ea99d7a980176c5d759064e79c9c
Melannie01 <melannie.arguedas.delgado@est.una.ac.cr>
2025-08-08 19:56:34 -0600

    Cambios radicales (tuve que cambiar el proyecto de las apis)

---
+++ BackEndBooky/APIs/JWT/JwtConfig.cs
@@ -0,0 +1,31 @@
+﻿using Owin;
+using System.Configuration;
+using System.Text;
+using System;
+using Microsoft.Owin.Security.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+public static class JwtConfig
+{
+    public static void ConfigureAuth(IAppBuilder app)
+    {
+        var issuer = ConfigurationManager.AppSettings["JWT:Issuer"];
+        var secretKey = ConfigurationManager.AppSettings["JWT:SecretKey"];
+
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+        {
+            AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active,
+            TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidIssuer = issuer,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero // Para que la expiración del token sea más estricta
+            }
+        });
+    }
+}
---
+++ BackEndBooky/Entities/Request/ReqBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Request
+{
+    public class ReqBase
+    {
+        public string token { get; set; }
+        public string fechaVencimiento { get; set; }   
+
+    }
+}
