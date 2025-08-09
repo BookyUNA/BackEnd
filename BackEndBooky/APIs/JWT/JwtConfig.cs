0ece89153ed1bb4c62dfd3cb3b279725df4b5d38
Melannie01 <melannie.arguedas.delgado@est.una.ac.cr>
2025-08-08 20:46:30 -0600

    Inicio de sesión

--- BackEndBooky/APIs/JWT/JwtConfig.cs
+++ BackEndBooky/APIs/JWT/JwtConfig.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System;
 using Microsoft.Owin.Security.Jwt;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 public static class JwtConfig
 {
@@ -27,5 +29,31 @@ public static class JwtConfig
                 ClockSkew = TimeSpan.Zero // Para que la expiración del token sea más estricta
             }
         });
+    }
+
+    public static string GenerarToken(int idUsuario, string rol)
+    {
+        var issuer = ConfigurationManager.AppSettings["JWT:Issuer"];
+        var secretKey = ConfigurationManager.AppSettings["JWT:SecretKey"];
+        var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+        var claims = new[]
+        {
+                new Claim("IdUsuario", idUsuario.ToString()),
+                new Claim(ClaimTypes.Role, rol),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) // identificador único del token
+            };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: issuer,
+            audience: null,
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
