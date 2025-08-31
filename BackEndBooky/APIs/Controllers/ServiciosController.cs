3a5df1da6f5228c68a2cda6f093fafe9043dc25c
Melannie01 <melannie.arguedas.delgado@est.una.ac.cr>
2025-08-30 18:05:48 -0600

    Cambios

---
+++ BackEndBooky/APIs/Controllers/ServiciosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIs.Controllers
+{
+    public class ServiciosController : Controller
+    {
+        // GET: Servicios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
