e8ee5d923f24b07e53edb21d73230d6bca855577
mariajesusc <maria.cespedes.a@est.una.ac.cr>
2025-09-27 20:47:26 -0600

    Implementación Administración de Citas

---
+++ BackEndBooky/Entities/Response/ResListarCitasCliente.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Response
+{
+    public class ResListarCitasCliente : ResBase
+    {
+        public List<CitaCliente> Citas { get; set; }
+    }
+}
---
+++ BackEndBooky/Entities/Response/ResListarCitasProfesional.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Response
+{
+    public class ResListarCitasProfesional : ResBase
+    {
+        public List<CitaProfesional> Citas { get; set; }
+    }
+}
